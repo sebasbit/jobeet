67b874317da972ac98a8ca7c9664983368ae904a
sebasbit <sebastian.torres0426@gmail.com>
2022-06-22 11:08:25 -0500

    Dia 8: probar clase Jobeet para quitar acentos

--- test/unit/JobeetTest.php
+++ test/unit/JobeetTest.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__FILE__).'/../bootstrap/unit.php';
 
-$t = new lime_test(7);
+$t = new lime_test(9);
 
 $t->comment('::slugify()');
 $t->is(Jobeet::slugify('Sensio'), 'sensio', 'It converts all characters to lower case');
@@ -12,3 +12,13 @@ $t->is(Jobeet::slugify('  sensio'), 'sensio', 'It removes - at the beginning of 
 $t->is(Jobeet::slugify('sensio  '), 'sensio', 'It removes - at the end of a string');
 $t->is(Jobeet::slugify('paris,france'), 'paris-france', 'It replaces non-ASCII characters by a -');
 $t->is(Jobeet::slugify(''), 'n-a', 'It converts the empty string to n-a');
+$t->is(Jobeet::slugify(' - '), 'n-a', 'It converts a string that only contains non-ASCII characters to n-a');
+
+if (function_exists('iconv'))
+{
+  $t->is(Jobeet::slugify('Développeur Web'), 'developpeur-web', 'It removes accents');
+}
+else
+{
+  $t->skip('It removes accents - iconv not installed');
+}
