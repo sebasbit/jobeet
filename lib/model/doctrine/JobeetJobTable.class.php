79da4108b4d49336315231b17287c6c7044382eb
sebasbit <sebastian.torres0426@gmail.com>
2022-06-22 10:03:56 -0500

    Dia 7: redireccionar a categoria desde trabajo

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -17,7 +17,24 @@ class JobeetJobTable extends Doctrine_Table
       return Doctrine_Core::getTable('JobeetJob');
   }
 
+  public function retrieveActiveJob(Doctrine_Query $q)
+  {
+    $q->andWhere('a.expires_at > ?', date('Y-m-d h:i:s', time()));
+
+    return $q->fetchOne();
+  }
+
   public function getActiveJobs(Doctrine_Query $q = null)
+  {
+    return $this->addActiveJobsQuery($q)->execute();
+  }
+
+  public function countActiveJobs(Doctrine_Query $q = null)
+  {
+    return $this->addActiveJobsQuery($q)->count();
+  }
+
+  public function addActiveJobsQuery(Doctrine_Query $q = null)
   {
     if (is_null($q))
     {
@@ -25,16 +42,11 @@ class JobeetJobTable extends Doctrine_Table
         ->from('JobeetJob j');
     }
 
-    $q->andWhere('j.expires_at > ?', date('Y-m-d h:i:s', time()))
-      ->addOrderBy('j.expires_at DESC');
+    $alias = $q->getRootAlias();
 
-    return $q->execute();
-  }
+    $q->andWhere($alias . '.expires_at > ?', date('Y-m-d h:i:s', time()))
+      ->addOrderBy($alias . '.expires_at DESC');
 
-  public function retrieveActiveJob(Doctrine_Query $q)
-  {
-    $q->andWhere('a.expires_at > ?', date('Y-m-d h:i:s', time()));
-
-    return $q->fetchOne();
+    return $q;
   }
 }
