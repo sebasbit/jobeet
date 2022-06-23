836110fd72460f9768c96527c9ab7b9f80796010
sebasbit <sebastian.torres0426@gmail.com>
2022-06-23 16:46:27 -0500

    Dia 14: agregar feed para trabajos y categorias

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -7,6 +7,7 @@
         Jobeet - Your best job board
       <?php endif; ?>
     </title>
+    <link rel="alternate" type="application/atom+xml" title="Latest Jobs" href="<?php echo url_for('job', array('sf_format' => 'atom'), true) ?>" />
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_javascripts() ?>
     <?php include_stylesheets() ?>
@@ -78,7 +79,7 @@
           </span>
           <ul>
             <li><a href="">About Jobeet</a></li>
-            <li class="feed"><a href="">Full feed</a></li>
+            <li class="feed"><a href="<?php echo url_for('job', array('sf_format' => 'atom')) ?>">Full feed</a></li>
             <li><a href="">Jobeet API</a></li>
             <li class="last"><a href="">Affiliates</a></li>
           </ul>
