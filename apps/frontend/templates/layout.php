e8405c8a23bd6498b5ac109b7f59a845dab49bdf
sebasbit <sebastian.torres0426@gmail.com>
2022-06-24 15:49:38 -0500

    Dia 18: implementar la barra de busqueda con AJAX

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -10,6 +10,7 @@
     <link rel="alternate" type="application/atom+xml" title="Latest Jobs" href="<?php echo url_for('job', array('sf_format' => 'atom'), true) ?>" />
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php use_javascript('jquery-3.6.0.min.js') ?>
+    <?php use_javascript('search.js') ?>
     <?php include_javascripts() ?>
     <?php include_stylesheets() ?>
   </head>
@@ -34,6 +35,7 @@
               <form action="<?php echo url_for('job_search') ?>" method="get">
                 <input type="text" name="query" value="<?php echo $sf_request->getParameter('query') ?>" id="search_keywords" />
                 <input type="submit" value="search" />
+                <img id="loader" src="/legacy/images/loader.gif" style="vertical-align: middle; display: none" />
                 <div class="help">
                   Enter some keywords (city, country, position, ...)
                 </div>
