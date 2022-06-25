85dbebbd77cf5e3f7ba3485f0ca536156a30e621
sebasbit <sebastian.torres0426@gmail.com>
2022-06-25 10:43:55 -0500

    Dia 19: agregar sfFormExtraPlugin

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -11,7 +11,8 @@ class ProjectConfiguration extends sfProjectConfiguration
   {
     $this->enablePlugins(array(
       'sfDoctrinePlugin',
-      'sfDoctrineGuardPlugin'
+      'sfDoctrineGuardPlugin',
+      'sfFormExtraPlugin'
     ));
   }
 
