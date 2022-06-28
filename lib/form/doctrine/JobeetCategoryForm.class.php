4a2ab8a3d798b984915763226f6db22f2bf88054
sebasbit <sebastian.torres0426@gmail.com>
2022-06-28 09:48:30 -0500

    Dia 19: crear una categoria con su traduccion

--- lib/form/doctrine/JobeetCategoryForm.class.php
+++ lib/form/doctrine/JobeetCategoryForm.class.php
@@ -12,6 +12,13 @@ class JobeetCategoryForm extends BaseJobeetCategoryForm
 {
   public function configure()
   {
-    unset($this['created_at'], $this['updated_at'], $this['jobeet_affiliates_list']);
+    unset(
+      $this['created_at'], $this['updated_at'],
+      $this['jobeet_affiliates_list']
+    );
+
+    $this->embedI18n(array('en', 'es'));
+    $this->widgetSchema->setLabel('en', 'English');
+    $this->widgetSchema->setLabel('es', 'Español');
   }
 }
