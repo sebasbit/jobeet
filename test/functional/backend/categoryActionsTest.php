d1479b037824a67d062e37ae022d41c837d4abdc
sebasbit <sebastian.torres0426@gmail.com>
2022-06-28 10:00:33 -0500

    Omitir pruebas funcionales vacias

--- test/functional/backend/categoryActionsTest.php
+++ test/functional/backend/categoryActionsTest.php
@@ -4,4 +4,4 @@ include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
 $browser = new sfTestFunctional(new sfBrowser());
 
-$browser->info('skip categoryActionsTest...');
+$browser->info('skip backend/categoryActionsTest...');
--- test/functional/frontend/languageActionsTest.php
+++ test/functional/frontend/languageActionsTest.php
@@ -4,16 +4,4 @@ include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
 $browser = new sfTestFunctional(new sfBrowser());
 
-$browser->
-  get('/language/index')->
-
-  with('request')->begin()->
-    isParameter('module', 'language')->
-    isParameter('action', 'index')->
-  end()->
-
-  with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
-  end()
-;
+$browser->info('skip frontend/languageActionsTest...');
